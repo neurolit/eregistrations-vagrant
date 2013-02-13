eacb2f81cb38d98bd7381839eb4acc60dffa29e1
neurolit <neurolit@gmail.com>
2013-02-13 18:05:24 +0100

    Added MongoDB install

--- manifests/default.pp
+++ manifests/default.pp
@@ -20,4 +20,24 @@ class nodejs {
 	}
 }
 
+class mongodb {
+	package { 'mongodb-10gen':
+		ensure => present,
+	}
+
+	file { '/etc/apt/sources.list.d/10gen.list':
+		content => 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen',
+		mode => '0644',
+		owner => 'root',
+		group => 'root',
+	}
+
+	exec { 'apt-key add mongodb':
+		command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10',
+	}
+
+	Exec['apt-key add mongodb'] -> File['/etc/apt/sources.list.d/10gen.list'] -> Exec['apt-get update'] -> Package['mongodb-10gen']
+}
+
 include nodejs
+include mongodb
